e24bddee909d37cd2feef8562b302e31d9dcaad8
Caeldemaria <138541178+Caeldemaria@users.noreply.github.com>
2023-07-19 16:03:52 -0300

    final

---
+++ 01/index.js
@@ -0,0 +1,13 @@
+const jogada1 = "Pedra"
+const jogada2 = "tesoura"
+const vencedora = "pedra"
+if (vencedora === "pedra") {
+    console.log(vencedora)
+
+}
+
+else {
+    console.log('empate')
+
+}
+

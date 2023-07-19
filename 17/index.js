3a97a4f4c329877b601d0b4aab1960947c44997f
Caeldemaria <138541178+Caeldemaria@users.noreply.github.com>
2023-07-19 16:20:15 -0300

    final

---
+++ 17/index.js
@@ -0,0 +1,29 @@
+//valor do produto comprado.
+const valorDoProduto = 100000;
+
+//quantidade de parcelas
+const quantidadeDoParcelamento = 10;
+
+//valor pago
+const valorPago = 300;
+if (valorPago === 100) {
+    console.log('Restam 9 parcelas de R$100')
+}
+else if (valorPago === 200) {
+    console.log('Restam 8 parcelas de R$100')
+}
+else if (valorPago === 300) {
+    console.log('Restam 7 parcelas de R$100')
+} else if (valorPago === 400) {
+    console.log('Restam 6 parcelas de R$100')
+} else if (valorPago === 500) {
+    console.log('Restam 5 parcelas de R$100')
+} else if (valorPago === 600) {
+    console.log('Restam 4 parcelas de R$100')
+} else if (valorPago === 700) {
+    console.log('Restam 3 parcelas de R$100')
+} else if (valorPago === 800) {
+    console.log('Restam 2 parcelas de R$100')
+} else if (valorPago === 900) {
+    console.log('Restam 1 parcelas de R$100')
+} 

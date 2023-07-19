d8e7617530f37ecd4eda4367bd6da3032119d371
Caeldemaria <138541178+Caeldemaria@users.noreply.github.com>
2023-07-19 16:05:37 -0300

    Final

---
+++ 02/index.js
@@ -0,0 +1,10 @@
+const jogada1 = 5;
+const jogada2 = 3;
+const vencedora = (jogada1 + jogada2) / 2
+if (vencedora % 2 === 0) {
+    console.log('par')
+}
+else {
+    console.log("impar")
+}
+//seu código aqui
